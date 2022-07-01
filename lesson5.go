bdd49b28ea4d22a12f05b0723fb7c0a59a0e12d5
Олег А <97842702+15071986@users.noreply.github.com>
2022-07-01 21:31:35 +0300

    Add files via upload

---
+++ lesson5.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fib(n int, carta map[int]int) int {
+
+	if val, ok := carta[n]; ok {
+		return val
+	}
+	carta[n] = fib(n-1, carta) + fib(n-2, carta)
+
+	return carta[n]
+
+}
+
+func main() {
+	var n int
+	carta := map[int]int{
+
+		0: 0,
+		1: 1,
+	}
+	fmt.Println("Ведите значение: ")
+	fmt.Scanln(&n)
+	fmt.Println("Результат: ", fib(n, carta))
+	fmt.Println(carta)
+}
